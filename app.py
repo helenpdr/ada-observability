5cbc1e121c8ed49280c7967616a7f61df8b188b0
helenpdr <helenpdr@gmail.com>
2023-03-03 19:45:31 -0300

    Criação de Graylog, Mongodb e Elasticsearch.

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+import http.client
 import random
 import time
 
 from flask import Flask, render_template
 import prometheus_client as prom
 from prometheus_flask_exporter import PrometheusMetrics
+import logging
 
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -19,21 +24,27 @@ def parametros_endpoint():
 
 @app.route('/renda-fixa')
 def renda_fixa():
+    app.logger.info("Acessando Renda Fixa!")
     parametros_endpoint()
+    if random.randint(0, 1) == 0:
+        return http.client.BAD_REQUEST
     return render_template('lista.html', titulo='Renda Fixa')
 
 @app.route('/renda-variavel')
 def renda_variavel():
+    app.logger.info("Acessando Renda Variável!")
     parametros_endpoint()
     return render_template('lista.html', titulo='Renda Variável')
 
 @app.route('/cripto')
 def cripto():
+    app.logger.info("Acessando Cripto!")
     parametros_endpoint()
     return render_template('lista.html', titulo='Cripto')
 
 @app.route('/fii')
 def fiis():
+    app.logger.info("Acessando Fii!")
     parametros_endpoint()
     return render_template('lista.html', titulo='FIIs')
 
